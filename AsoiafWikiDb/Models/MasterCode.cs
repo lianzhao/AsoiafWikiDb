6b563ab043cd2f7650944cd126bf0bba233233ee
lianzhao <knife.fe@gmail.com>
2014-11-28 15:02:49 +0800

    introduce master code entity

---
+++ AsoiafWikiDb/Models/MasterCode.cs
@@ -0,0 +1,13 @@
+﻿namespace AsoiafWikiDb.Models
+{
+    public class MasterCode
+    {
+        public virtual int Id { get; set; }
+
+        public virtual string Key { get; set; }
+
+        public virtual string Value { get; set; }
+
+        public virtual string Category { get; set; }
+    }
+}
